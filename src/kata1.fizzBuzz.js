5688965a69f31f74bb904973c280a0bf85dc0079
Alex White <pixeljuggle@gmail.com>
2021-03-09 19:04:38 +0000

    Completed fizzBuzz & booleanToWord

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,15 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+if ( number % 5 === 0 && number % 3 === 0 ){
+  return 'FizzBuzz'
+}
 
+if ( number % 5 === 0 ) {
+  return 'Buzz'
+}
+
+if ( number % 3 === 0 ) {
+  return 'Fizz'
+}
+return number
+};
 module.exports = fizzBuzz;
