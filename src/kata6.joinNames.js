49f23240d8033eb3335ad99066efd39dd9fb8996
Alex White <pixeljuggle@gmail.com>
2021-03-10 15:05:12 +0000

    Completed joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,10 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  return namesObj.reduce( (a, b, i) =>{
+    if ( ( i + 1 ) === namesObj.length ){
+      return (a.name || a) + " & " + b.name
+    }
+    return (a.name || a) + ", " + b.name
+  })
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,8 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{name:'Bart'},{name:'Lisa'},{ name:'Maggie'}])).toBe('Bart, Lisa & Maggie');
+    expect(joinNames([{name:'Bart'},{name:'Lisa'},{ name:'Maggie'},{ name:'Homer'},{ name:'Marge'}])).toBe('Bart, Lisa, Maggie, Homer & Marge');
+  });
 });
