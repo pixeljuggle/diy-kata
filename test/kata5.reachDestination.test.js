a57a89077fcf380d41652d3c5a29e08588bbe9fa
Alex White <pixeljuggle@gmail.com>
2021-03-10 14:46:12 +0000

    Completed reachDestination

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,10 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe('should be there in 4.5 hours.');
+    expect(reachDestination(46, 10)).toBe('should be there in 4.5 hours.');
+    expect(reachDestination(14, 30)).toBe('should be there in 0.5 hours.');
+    expect(reachDestination(45, 15)).toBe('should be there in 3 hours.');
+  });
 });
