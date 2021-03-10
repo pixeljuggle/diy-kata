38ca9a466283551aa4ec4a772fa70c3ebde882b2
Alex White <pixeljuggle@gmail.com>
2021-03-10 15:19:43 +0000

    Refactoring tests

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -3,14 +3,21 @@ const { getEmployerRole } = require("../src");
 describe("getEmployerRole", () => {
   it("returns the employee's role in the company", () => {
     const employees=[{name:"Satti",role:"Developer"},{name:"Jenny",role:"Sales Associate"},{name:"Javid",role:"Human Recommended Reading Assistant"}];
-    expect(getEmployerRole('Javid', employees)).toBe('Human Recommended Reading Assistant');
+    const expectedResult = 'Human Recommended Reading Assistant'
+    const actualResult = getEmployerRole('Javid', employees)
+    expect(actualResult).toBe(expectedResult);
   });
   it("returns the employee's role in the company", () => {
     const employees=[{name:"Satti",role:"Developer"},{name:"Jenny",role:"Sales Associate"},{name:"Javid",role:"Human Recommended Reading Assistant"}];
-    expect(getEmployerRole('Satti', employees)).toBe('Developer');
+    const expectedResult = 'Developer'
+    const actualResult = getEmployerRole('Satti', employees)
+    expect(actualResult).toBe(expectedResult);
   });
   it("returns the employee's role in the company", () => {
     const employees=[{name:"Satti",role:"Developer"},{name:"Jenny",role:"Sales Associate"},{name:"Javid",role:"Human Recommended Reading Assistant"}];
-    expect(getEmployerRole('Jenny', employees)).toBe('Sales Associate');
+    const expectedResult = 'Sales Associate'
+    const actualResult = getEmployerRole('Jenny', employees)
+    expect(actualResult).toBe(expectedResult);
   });
 });
+
